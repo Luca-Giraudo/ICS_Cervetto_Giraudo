48cac3a84117825444251b56f3390d3b3e030444
Luca-Giraudo <lucagiraudo2002@gmail.com>
2024-08-28 22:54:50 -0300

    Segundo Commit
    Creacion de modelos y migraciones. Vista problematica eliminada.

--- django_example_app-master/quotes/models.py
+++ django_example_app-master/quotes/models.py
@@ -1,11 +1,33 @@
 from django.db import models
+from localflavor.ar.ar_provinces import PROVINCE_CHOICES
 
-# Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=30)
-    price = models.FloatField()
-    provider = models.ForeignKey('Provider', null=True, on_delete=models.CASCADE)
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    perfil_empresa = models.ForeignKey('PerfilEmpresa', on_delete=models.CASCADE, null=True, blank=True)
+    perfil_usuario = models.ForeignKey('PerfilUsuario', on_delete=models.CASCADE, null=True, blank=True)
+    favoritos = models.ForeignKey('Favorito', on_delete=models.CASCADE, null=True, blank=True)
 
-class Provider(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=30)
+    def __str__(self):
+        return self.nombre
+
+class PerfilEmpresa(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=144)
+    enlaces = models.URLField(max_length=200, blank=True, null=True)
+    localidad = models.CharField(max_length=100, choices=PROVINCE_CHOICES)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
+
+class PerfilUsuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    localidad = models.CharField(max_length=100, choices=PROVINCE_CHOICES)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
+
+class Favorito(models.Model):
+    servicio = models.ForeignKey('PerfilEmpresa', on_delete=models.CASCADE, null=True, blank=True)
--- django_example_app-master/quotes/views.py
+++ django_example_app-master/quotes/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from quotes.models import Item
 
 # Create your views here.
 def main(request):
-    context = {
-        "items": Item.objects.all()
-    }
-    return render(request, 'quotes/main.html', context)
+    return render(request, 'quotes/main.html')
+
